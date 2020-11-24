a7172068c0ce7e9daa62a1f601b7d6c2867e9fec
Lucas Campos <lucascampos.ads@gmail.com>
2020-11-24 20:39:08 -0300

    docs: Add pages

---
+++ src/pages/FormTag.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/global.css';
+
+function FormTag() {
+  return (
+    <h1>Hello World - FormTag.</h1>
+  );
+}
+
+export default FormTag;
---
+++ src/pages/Label.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Label() {
+  return (
+    <h1>Label page.</h1>
+  );
+}
+
+export default Label;
