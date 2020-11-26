b4aa7b949e1f08c612082c9f89e06a77d351700e
Lucas Campos <lucascampos.ads@gmail.com>
2020-11-25 22:02:38 -0300

    feat: Search CEP

--- src/pages/CreateTag.tsx
+++ src/pages/CreateTag.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useLayoutEffect, FormEvent } from 'react';
 import { FiSearch, FiTag } from 'react-icons/fi';
+import axios from 'axios';
 
 import '../styles/global.css';
 import '../styles/pages/create-tag.css';
 
 function CreateTag() {
+  const [cep, setCep] = useState('');
+  const [street, setStreet] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+
+  useLayoutEffect(() => {
+    document.getElementById('name')?.focus();
+  }, []);
+
+  async function handleSearchCEP(event: FormEvent) {
+    event.preventDefault();
+
+    if (cep) {
+      axios
+        .get(`https://brasilapi.com.br/api/cep/v1/${cep}`)
+        .then((response) => {
+          setStreet(response.data.street);
+          setNeighborhood(response.data.neighborhood);
+          setCity(response.data.city);
+          setState(response.data.state);
+        })
+        .catch((error) => {
+          setStreet('');
+          setNeighborhood('');
+          setCity('');
+          setState('');
+
+          alert('Cep inexistente.');
+          document.getElementById('cep')?.focus();
+        });
+    }
+  }
+
   return (
     <div className="page-create-tag">
       <main>
@@ -20,18 +55,26 @@ function CreateTag() {
             <div className="input-block-cep">
               <div className="input-block">
                 <label htmlFor="cep">CEP</label>
-                <input id="cep" />
+                <input
+                  id="cep"
+                  value={cep}
+                  onChange={(event) => setCep(event.target.value)}
+                />
               </div>
 
-              <button className="cep-button">
+              <button className="cep-button" onClick={handleSearchCEP}>
                 Pesquisar &nbsp;
                 <FiSearch size={18} color="#ffffff" />
-                </button>
+              </button>
             </div>
 
             <div className="input-block">
               <label htmlFor="rua">Rua</label>
-              <input id="rua" />
+              <input
+                id="rua"
+                value={street}
+                onChange={(event) => setStreet(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
@@ -46,17 +89,29 @@ function CreateTag() {
 
             <div className="input-block">
               <label htmlFor="bairro">Bairro</label>
-              <input id="bairro" />
+              <input
+                id="bairro"
+                value={neighborhood}
+                onChange={(event) => setNeighborhood(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="cidade">Cidade</label>
-              <input id="cidade" />
+              <input
+                id="cidade"
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="estado">Estado</label>
-              <input id="estado" />
+              <input
+                id="estado"
+                value={state}
+                onChange={(event) => setState(event.target.value)}
+              />
             </div>
 
             <button className="confirm-button" type="submit">
